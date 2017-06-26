3fa4d8810dfabdfe9741e2a911cb2f329aa6de9b
DiegoColli <diegocostacolli@gmail.com>
2017-06-26 19:54:40 -0300

    First

---
+++ src/main/java/demo/repository/PersonRepository.java
@@ -0,0 +1,15 @@
+package demo.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import demo.model.Person;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+	
+	Collection<Person> findAll();
+
+}
