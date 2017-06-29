ec19ca855f07d157e573531473b1c8678b773d27
DiegoColli <diegocostacolli@gmail.com>
2017-06-29 14:42:23 -0300

    De alguma maneira algo funciona assim

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,7 +1,6 @@
 package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 //@EnableResourceServer
